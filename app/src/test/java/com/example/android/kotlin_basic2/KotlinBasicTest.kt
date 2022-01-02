f3249347f0d4ba1515214668f97c08dc7b5c5058
Your Name <you@example.com>
2022-01-02 23:06:04 +0800

    is关键字类型判断 as类型强转 扩展函数()没有父类引用指向子类对象

--- app/src/test/java/com/example/android/kotlin_basic2/KotlinBasicTest.kt
+++ app/src/test/java/com/example/android/kotlin_basic2/KotlinBasicTest.kt
@@ -12,22 +12,75 @@ import org.junit.Assert.*
 const val TYPE_1 = 1
 const val TYPE_2 = 2
 
+open class Person {
+    open fun doAction(){
+        println("person")
+    }
+}
+class Man : Person() {
+    override fun doAction(){
+        println("Man")
+    }
+
+    fun jiaoji() {
+        println("jiaoji")
+    }
+}
+
+fun Person.fuck() {
+    println("Person fuck ")
+}
+
+/**
+ * 扩展函数是没有动态运行期的哈
+ */
+fun Man.fuck() {
+    println("Man fuck")
+}
+
+
+
+fun whenTest(type: Int) {
+    when (type) {
+        TYPE_1 -> println("type 1")
+        TYPE_2 -> println("type 2")
+        else -> println("other")
+    }
+}
+
 class KotlinBasicTest {
+    // 只有在class中才有多个@Test入口
     @Test
-    fun addition_isCorrect() {
-        println("xxxx")
+    fun testAS() {
+        val man = Man()
+        val person = Person()
+//        man.doAction()
+        xxxx(person)
+        xxxx(man)
     }
 
     @Test
-    fun dddd() {
-        whenTest(5)
+    fun testAS2() {
+        val person = Man()
+        isTest(person)
     }
 
-    fun whenTest(type: Int) {
-        when (type) {
-            TYPE_1 -> println("type 1")
-            TYPE_2 -> println("type 2")
-            else -> println("other")
+    fun isTest(person: Person) {
+        if (person is Man) {
+            println("是男人")
+            (person as Man).jiaoji()
+        } else {
+            println("不是男人")
         }
+    }
+
+    fun xxxx(person: Person) {
+        person.doAction()
+        person.fuck()
+    }
+
+    @Test
+    fun testWhen() {
+        whenTest(5)
     }
 }
